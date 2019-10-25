c6a98ac41081bea343e4b8857b07b10e147b748f
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-10-25 08:23:42 -0500

    example of repetition in strings

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -8,3 +8,19 @@ firstname = "Billy"
 lastname =  "Bobby"
 
 fullName = firstname + lastname
+
+print(fullName)
+
+# Repetition
+#   repetition operator; *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + 'your boat')
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
+
